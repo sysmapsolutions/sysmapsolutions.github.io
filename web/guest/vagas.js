ff785180589592be7e8a88278dad9958f745d03a
marciofranca <msfranca@gmail.com>
2017-03-18 18:08:40 -0300

    commit

--- web/guest/vagas.js
+++ web/guest/vagas.js
@@ -3,8 +3,9 @@ var SysMap_VAGA_URL_PREFIX = "https://sysmapsolutions.github.io/vagas/{0}.json";
 function SysMap_Vaga(codigo) {
 	this.codigo = codigo;
 
-	function vagaRetornada(){
-		console.info(arguments);
+	function vagaRetornada(dados){
+		this.dados = dados;
+		console.info(this);
 	}
 
 	$.ajax(SysMap_VAGA_URL_PREFIX.format(codigo)).done(vagaRetornada);
@@ -18,5 +19,4 @@ SysMap_Vaga.prototype.render = function() {
 {
 	var codigo = window.location.hash.substring(1);
 	var vaga = new SysMap_Vaga(codigo);
-	console.info(vaga);
 }
