875e2b76353fdc5e0a30e3e178732d69a6f58b63
marciofranca <msfranca@gmail.com>
2017-03-26 10:47:53 -0300

    commit

--- script/SysMap_Oportunidade.js
+++ script/SysMap_Oportunidade.js
@@ -1,1 +1,13 @@
 document.write("<div id='sm-conteudo' class='page-container page-internal oportunidades'/> ");
+
+$.get("template.mst", function(template) {
+	var vaga = {
+		dados: {
+			codigo: "1000",
+			nome: "Consultor Técnico de Pré-Venda"
+		}
+	};
+
+	var resultado = Mustache.render(template, vaga);
+	$('#sm-conteudo').html(resultado);
+});
