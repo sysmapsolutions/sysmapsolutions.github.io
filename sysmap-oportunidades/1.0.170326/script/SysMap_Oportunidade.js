134e172d5a3dac4d26f3b5dfb3f565d20d839bce
marciofranca <msfranca@gmail.com>
2017-03-26 11:13:35 -0300

    commit

---
+++ sysmap-oportunidades/1.0.170326/script/SysMap_Oportunidade.js
@@ -0,0 +1,20 @@
+var SysMap_SCRIPT_URL = "https://sysmapsolutions.github.io/script/";
+var SysMap_TEMPLATE_URL = "https://sysmapsolutions.github.io/template/";
+
+function SysMap_Template(nome){
+	return SysMap_TEMPLATE_URL + nome + ".mst";
+}
+
+document.write("<div id='sm-conteudo' class='page-container page-internal oportunidades'/> ");
+
+$.get(SysMap_Template("SysMap_Oportunidade"), function(template) {
+	var vaga = {
+		dados: {
+			codigo: "1000",
+			nome: "Consultor Técnico de Pré-Venda"
+		}
+	};
+
+	var resultado = Mustache.render(template, vaga);
+	$('#sm-conteudo').html(resultado);
+});
