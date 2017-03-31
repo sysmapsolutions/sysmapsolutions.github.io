7b7835361b8d041ddca36d2f00454dd73e85d5a3
marciofranca <msfranca@gmail.com>
2017-03-31 17:33:02 -0300

    commit

---
+++ sysmap-oportunidades/1.0.170326/script/SysMap_Analytics.js
@@ -0,0 +1,109 @@
+var SysMap_Analytics_TRACKER = "SysMap_Vagas";
+var SysMap_Analytics_ID = "UA-93986043-1";
+
+SysMap_Analytics = (function () {
+	var id = "";
+
+	ga("create", SysMap_Analytics_ID, "auto", SysMap_Analytics_TRACKER);
+
+	function enviar(comando, config){
+		ga(SysMap_Analytics_TRACKER + "." + comando, config);
+	}
+
+	function enviarEvento(acao, texto){
+		enviar("send", {
+			hitType: "event",
+			eventCategory: "SysMap Vagas",
+			eventAction: acao,
+			eventLabel: texto
+		});
+	}
+
+	function enviarEventoDados(acao){
+		var texto;
+
+		if(SysMap_Candidato.email){
+			enviar("set", {
+				"userId": SysMap_Candidato.email
+			});
+
+			texto = SysMap_Analytics.id + "/email/" + SysMap_Candidato.email;
+			enviarEvento(acao, texto);
+		}
+		if(SysMap_Candidato.dados){
+			if(SysMap_Candidato.dados.nome){
+				texto = SysMap_Analytics.id + "/nome/" + SysMap_Candidato.dados.nome;
+				enviarEvento(acao, texto);
+			}
+			if(SysMap_Candidato.dados.telefone){
+				texto = SysMap_Analytics.id + "/fone/" + SysMap_Candidato.dados.telefone;
+				enviarEvento(acao, texto);
+			}
+			if(SysMap_Candidato.dados.linkedin){
+				texto = SysMap_Analytics.id + SysMap_Candidato.dados.linkedin;
+				enviarEvento(acao, texto);
+			}
+		}
+		if(!texto){
+			texto = SysMap_Analytics.id;
+			enviarEvento(acao, texto);
+		}
+	}
+
+	function enviarVaga(){
+		if(SysMap_Vaga.dados.nome){
+			document.title = SysMap_Vaga.dados.nome + " (#" + SysMap_Vaga.codigo + ") - SysMap";
+		}else{
+			document.title = "Erro: Vaga Não Encontrada - SysMap";
+		}
+
+		if(SysMap_Vaga.codigo){
+			enviar("set", {
+				"page": "/vagas/" + SysMap_Vaga.codigo
+			});
+		}else{
+			enviar("set", {
+				"location": location.href
+			});
+		}
+
+		enviar("send", {
+			hitType: "pageview",
+			title: document.title
+		});
+
+		enviarCandidatoVeVaga();
+	}
+
+	function enviarCandidatoVeVaga(){
+		var acao = "viu vaga/" + SysMap_Vaga.codigo;
+		enviarEventoDados(acao);
+	}
+
+	function enviarCandidatoLinkedIn(){
+		var acao = "entrou com LinkedIn na vaga/" + SysMap_Vaga.codigo;
+		enviarEventoDados(acao);
+	}
+
+	function enviarCandidatoEmail(){
+		var acao = "preencheu e-mail na vaga/" + SysMap_Vaga.codigo;
+		enviarEventoDados(acao);
+	}
+
+	function enviarCandidatoSubmete(){
+		var acao = "se cadastrou na vaga/" + SysMap_Vaga.codigo;
+		enviarEventoDados(acao);
+	}
+
+	return {
+		id : id,
+		enviarVaga: enviarVaga,
+		enviarCandidatoLinkedIn: enviarCandidatoLinkedIn,
+		enviarCandidatoEmail: enviarCandidatoEmail,
+		enviarCandidatoSubmete: enviarCandidatoSubmete
+	}
+})();
+
+ga(function() {
+	SysMap_Analytics.id = ga.getByName(SysMap_Analytics_TRACKER).get("clientId");
+});
---
+++ sysmap-oportunidades/1.0.170326/script/SysMap_Vaga.js
@@ -0,0 +1,96 @@
+var SysMap_TEMPLATE_URL = "https://sysmapsolutions.github.io/sysmap-oportunidades/1.0.170326/template/{0}.mst";
+var SysMap_Vaga_URL = "https://sysmapsolutions.github.io/vagas/{0}.json";
+
+SysMap_Vaga = (function () {
+	this.codigo = "";
+	this.dados = {};
+
+	function vagaRetornada(dados){
+		this.dados = dados;
+		SysMap_Analytics.enviarVaga();
+		SysMap_Vaga_UI.atualizar();
+	}
+
+	function vagaNaoRetornada(){
+		SysMap_Analytics.enviarVaga();
+	}
+
+	function carregar(codigo){
+		this.codigo = codigo;
+		this.dados = {};
+
+		$.ajax({
+			url: SysMap_Vaga_URL.format(codigo),
+			context: this
+		}).done(vagaRetornada).fail(vagaNaoRetornada);
+	}
+
+	return {
+		carregar: carregar
+	}
+})();
+
+
+SysMap_Vaga_UI = (function () {
+	function atualizarCampo(seletor, campo){
+		$(seletor).html(SysMap_Vaga.dados[campo]);
+	}
+
+	function atualizarLista(seletor, campo){
+		var dados = SysMap_Vaga.dados[campo];
+
+		var ultimoIndice = -1;
+		var ultimoElemento;
+		$(seletor).each(function(i){
+			if(i < dados.length){
+				ultimoIndice = i;
+				ultimoElemento = $(this);
+				ultimoElemento.html(dados[ultimoIndice]);
+				ultimoElemento.show(0);
+			}else{
+				$(this).hide(0);
+			}
+		});
+
+		if(ultimoIndice >= 0){
+			while(ultimoIndice < dados.length - 1){
+				ultimoIndice++;
+				var clone = ultimoElemento.clone();
+				clone.html(dados[ultimoIndice]);
+				ultimoElemento.after(clone);
+				ultimoElemento = clone;
+			}
+		}
+	}
+
+
+	function atualizar(){
+		function SysMap_Template(nome){
+			return SysMap_Vaga_URL.format(nome);
+		}
+
+		$.get(SysMap_Template("SysMap_Oportunidade"), function(template) {
+			var resultado = Mustache.render(template, SysMap_Vaga);
+			$('#sm-conteudo').html(resultado);
+		});
+
+/*
+		atualizarCampo(".sysmap-vaga-codigo", "codigo");
+		atualizarCampo(".sysmap-vaga-nome", "nome");
+
+		atualizarLista(".sysmap-vaga-descricao", "descricao");
+		atualizarLista(".sysmap-vaga-responsabilidades", "responsabilidades");
+		atualizarLista(".sysmap-vaga-experiencias", "experiencias");
+		atualizarLista(".sysmap-vaga-atributos", "atributos");
+		atualizarLista(".sysmap-vaga-outros", "outros");
+*/
+	}
+
+	return {
+		atualizar: atualizar
+	}
+})();
+
+{
+	SysMap_Vaga.carregar(QueryString.codigo);
+}
